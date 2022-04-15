d7a83624bd08638f934c23c8836b0bfb94ef305b
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-04-15 17:38:23 -0300

    msg

---
+++ src/components/service/ServiceForm.js
@@ -0,0 +1,53 @@
+import {useState} from 'react';
+
+import Input from '../form/Input';
+import SubmitButton from '../form/SubmitButton';
+
+import styles from "../project/ProjectForm.module.css";
+
+function ServiceForm( { handleSubmit, btnText, projectData } ){
+
+    const [service, setService] = useState({});
+
+    function submit(e){
+        e.preventDefault();
+        projectData.services.push(service);
+        handleSubmit(projectData);
+    }
+
+    function handleChange(e){
+        setService({...service, [e.target.name] : e.target.value})
+    }
+
+    return(
+        <form onSubmit={submit} className={styles.form}>
+            <Input 
+                type="text"
+                text="Nome do Servico"
+                name="name"
+                placeholder="Insira o nome do servico"
+                handleOnChange={handleChange}
+            />
+
+            <Input 
+                type="number"
+                text="Custo do Servico"
+                name="cost"
+                placeholder="Insira o valor total"
+                handleOnChange={handleChange}
+            />
+
+            <Input 
+                type="text"
+                text="Descricao do Servico"
+                name="description"
+                placeholder="Descreva o servico"
+                handleOnChange={handleChange}
+            />
+
+            <SubmitButton text={btnText} />
+        </form>
+    );
+}
+
+export default ServiceForm;
